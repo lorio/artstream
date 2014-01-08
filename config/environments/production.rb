863f3fca2d9b42577e67e5641d23f8cce7c8f2a9
lorio <lori@loriortiz.com>
2014-01-08 09:16:15 -0500

    Tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Artstream::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -59,7 +59,7 @@ Artstream::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( boots_custom.css.scss.erb )
+  config.assets.precompile += %w( boots_custom.css.scss.erb, /dev-assets/stream.svg )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
