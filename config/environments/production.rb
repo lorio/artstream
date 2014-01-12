44cd24a9ac32686d7234658477d4785ddc74ba4f
lorio <lori@loriortiz.com>
2014-01-12 14:58:52 -0500

    Add AWS config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,15 @@ Artstream::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Required for Devise
-   config.action_mailer.default_url_options = { :host => 'artstream.us' }
+  config.action_mailer.default_url_options = { :host => 'artstream.us' }
+  
+  #Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
