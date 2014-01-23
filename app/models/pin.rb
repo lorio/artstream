b4ca03dd9d6d38b6a839d4c65b87c2aee5603ed2
lorio <lori@loriortiz.com>
2014-01-23 16:25:30 -0500

    Style and image size for show

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,11 @@
 class Pin < ActiveRecord::Base
 				belongs_to :user
-				has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+				has_attached_file :image, :styles => { 
+																		:medium => "300x300>",
+																		:large => "100%>",																				 
+																		:thumb => "100x100>" }
 
 				validates :image, presence: true
+				#validates :content_type => { :content_type => ["image/jpg", "image/gif", "image/png", "image/jpeg"] }
   			validates :description, presence: true, length: { minimum: 3 }
 end
