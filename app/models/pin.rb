886cc163fc709e1c14166aca07ffaef07881f48e
lorio <lori@loriortiz.com>
2014-01-24 15:34:36 -0500

    Limit image file size and content types

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,11 +1,16 @@
 class Pin < ActiveRecord::Base
 				belongs_to :user
 				has_attached_file :image, :styles => { 
-																		:medium => "300x300>",
-																		:large => "100%>",																				 
+																		:medium => "300x300>",																			 
 																		:thumb => "100x100>" }
 
 				validates :image, presence: true
-				#validates :content_type => { :content_type => ["image/jpg", "image/gif", "image/png", "image/jpeg"] }
+													
+				validates_attachment_size :image,
+															:less_than => 2.megabytes,
+															:message => 'under 2MB only' 				
+  			validates_attachment_content_type :image, 
+                                    :content_type => /^image\/(png|gif|jpeg)/,
+                                    :message => 'only (png/gif/jpeg) images'
   			validates :description, presence: true, length: { minimum: 3 }
 end
